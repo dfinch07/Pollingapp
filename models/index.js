49401c72409bb48233fe80d4581b5b4ec70ae3d4
sujithjohn <johnsujith2000@gmail.com>
2019-02-17 07:39:26 -0500

    feb17

---
+++ models/index.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+mongoose.set('debug', true);
+mongoose.Promise = global.Promise;
+const MONGODB_URI = "mongodb://root:root123@ds015869.mlab.com:15869/pollapp"
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
+
+module.exports.User = require('./user');
+module.exports.Poll = require('./poll');
