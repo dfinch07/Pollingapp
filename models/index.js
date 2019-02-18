442327d68852a16566936ed95e05a63012c51ba6
johns <johns@wfu.edu>
2019-02-18 10:26:44 -0500

    feb18

--- models/index.js
+++ models/index.js
@@ -1,9 +1,8 @@
 const mongoose = require('mongoose');
 mongoose.set('debug', true);
 mongoose.Promise = global.Promise;
-//const MONGODB_URI = "mongodb://root:root123@ds015869.mlab.com:15869/pollapp"
+const MONGODB_URI = "mongodb://root:root123@ds015869.mlab.com:15869/pollapp"
 
-mongoose.connect(process.env.DATABASE, { useNewUrlParser: true });
-
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/pollApp")
 module.exports.User = require('./user');
 module.exports.Poll = require('./poll');
