1cc1434541cf5486e5130dec63613f3cf9b7e438
sujithjohn <johnsujith2000@gmail.com>
2019-02-18 07:14:00 -0500

    push

--- models/index.js
+++ models/index.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 mongoose.set('debug', true);
 mongoose.Promise = global.Promise;
-const MONGODB_URI = "mongodb://root:root123@ds015869.mlab.com:15869/pollapp"
+//const MONGODB_URI = "mongodb://root:root123@ds015869.mlab.com:15869/pollapp"
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
+mongoose.connect(process.env.DATABASE, { useNewUrlParser: true });
 
 module.exports.User = require('./user');
 module.exports.Poll = require('./poll');
