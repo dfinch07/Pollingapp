30df0cb91b5f30d019bfe32808e6d88e8e04bf7e
johns <johns@wfu.edu>
2019-02-19 16:32:48 -0500

    test

--- index.js
+++ index.js
@@ -35,14 +35,11 @@ app.use((req, res, next) => {
 });
 app.use(handle.error);
 
-if (process.env.NODE_ENV === 'production'){
-  //set a static folder
-  app.use(express.static('client/build'));
-  app.get("*", (req, res) => {
+app.get("*", (req, res) => {
      res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
+});
 
-}
+
 
 
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
