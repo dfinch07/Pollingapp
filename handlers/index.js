cb6e417a7dad8dc21c0104a51741da01f01df39c
sujithjohn <johnsujith2000@gmail.com>
2019-02-18 20:04:00 -0500

    te

--- handlers/index.js
+++ handlers/index.js
@@ -7,7 +7,7 @@ module.exports.error = (err, req, res, next) => {
   return res.status(err.status || 500).json({
     success: false,
     error: {
-      message: "This s the server" + err.message || 'Something went wrong.',
+      message: err.message || 'Something went wrong.',
     },
   });
 };
--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser');
 
 const routes = require('./routes');
 const handle = require('./handlers');
+const path = require('path')
 
 const app = express();
 const PORT = process.env.PORT || 4000;
@@ -23,8 +24,14 @@ app.use((req, res, next) => {
 });
 app.use(handle.error);
 
-//app.get("*", (req, res) => {
-  //  res.sendFile(path.join(__dirname, "client", "build", "public/index.html"));
-//});
+if(process.env.NODE_ENV === 'production'){
+  //set a static folder
+  app.use(express.static('client/build'));
+  app.get("*", (req, res) => {
+     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+
+}
+
 
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
